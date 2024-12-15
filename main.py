50d1949f8255180056c48b095996a94c0412cc48
knguyenngo <khuongnguyenngo@gmail.com>
2024-12-15 03:08:38 -0500

    Removed scrape_by_scroll, convert_soup, format_html, and grab_moves functions What - Removed functions that utilized BeautifulSoup - Removed BeautifulSoup import Why - Program now utilizes Selenium to scrape, removing the need for BeautifulSoup Testing - Ref -

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import scrape_functions as sf
 
 url = 'https://www.smogon.com/dex/rs/moves/'
 
-sf.s_scrape(url)
+sf.scrape_moves(url)
 
 
--- scrape_functions.py
+++ scrape_functions.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,78 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 import json
 import time
-
-# Return soup from file
-def convert_soup(file_name):
-    with open(file_name) as original:
-        soup = BeautifulSoup(original, 'html.parser')
-    return soup
-
-# Prettify html files for readability
-def format_html(soup, write_file):
-    with open(write_file, "w", encoding = 'utf-8') as formatted:
-        formatted.write(str(soup.prettify()))
-
-# Grab move data-reactid
-def grab_moves(soup):
-    move_links = soup.find_all('a', class_="MoveLink")
-    return move_links
-
-# Scrape smogon url by scrolling
-def scrape_by_scroll(url):
-    # Set options for Firefox to run in background
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-
-    # Run Firefox in background
-    driver = webdriver.Firefox(options=options)
-
-    # Testing purposes
-    captured_first_moves = []
-    captured_last_moves = []
-
-    try:
-        driver.get(url)
-        
-        # Wait for spinner to disappear
-        WebDriverWait(driver, 10).until(
-            EC.invisibility_of_element_located((By.CLASS_NAME, "spinner"))
-        )
-
-        # Begin scrolling
-        print("Scrolling to load all moves...")
-        num_scrolls = 0
-        scroll_lengths = [
-            705, 700, 700, 695, 700, 700, 700, 700, 700, 700,  # 0-9
-            695, 700, 700, 700, 700, 700, 695, 700           # 10-17
-        ]
-
-        while True:
-            # Wait for page to load
-            time.sleep(3)
-            # Grab current .html and convert to soup object
-            page_source = driver.page_source
-            soup = BeautifulSoup(page_source, "html.parser")
-
-            # Grab all move links
-            move_links = grab_moves(soup)
-            captured_first_moves.append(move_links[0].text)
-            captured_last_moves.append(move_links[-1].text)
-
-            # Stop scrolling when captured end of move list
-            if (num_scrolls == 18):
-                break
-            
-            # Determine scroll length for current iteration and scroll by length
-            scroll_length = scroll_lengths[num_scrolls]
-            driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_length)
-
-            num_scrolls = num_scrolls+1
-    finally:
-        driver.quit()
-    
-    return captured_first_moves, captured_last_moves
 
 # Scrape smogon url for move list
 def scrape_moves(url):
